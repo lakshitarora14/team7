9bad69769d4ba7d5f5bb6d7500b4cc431cc7ed2a
Lakshit Arora <lakshitarora14@gmail.com>
2020-01-13 17:20:22 +0530

    first commit

---
+++ src/main/java/com/training/teamseven/TeamsevenApplication.java
@@ -0,0 +1,13 @@
+package com.training.teamseven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsevenApplication.class, args);
+	}
+
+}
